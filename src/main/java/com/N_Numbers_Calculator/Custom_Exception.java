f26f1e984d77efb4713dc709d9103e515297385e
ansumanparida <69868185+ansumanparida@users.noreply.github.com>
2020-09-30 10:42:51 +0530

    no message

--- src/main/java/com/N_Numbers_Calculator/Custom_Exception.java
+++
@@ -1,9 +0,0 @@
-package com.N_Numbers_Calculator;
-
-@SuppressWarnings("serial")
-public class Custom_Exception extends Exception {
-	
-	public Custom_Exception (String msg) {
-		super (msg);
-	}
-}
