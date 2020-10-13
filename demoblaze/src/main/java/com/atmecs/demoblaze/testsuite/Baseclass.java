f42e7cf6988767163e62d40fbc04d0ef3e414009
ansumanparida <69868185+ansumanparida@users.noreply.github.com>
2020-10-13 21:51:33 +0530

    no message

--- demoblaze/src/main/java/com/atmecs/demoblaze/testsuite/Baseclass.java
+++ demoblaze/src/main/java/com/atmecs/demoblaze/testsuite/Baseclass.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -38,6 +39,9 @@ public class Baseclass {
 		}
 	
 		driver.get(weburl);
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "PHPTRAVELS booking script and system for hotels airline flights tours cars online application - PHPTRAVELS";
+		Assert.assertEquals(actualTitle, expectedTitle);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 }
